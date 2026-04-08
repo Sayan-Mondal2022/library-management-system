9268578bd866fb34a7630bbeba3cf20a2871bdbd
Sayan-Mondal2022 <sayanmondal5678@gmail.com>
2026-04-08 22:24:11 +0530

    Updated DBConnection.java file, added dependencies to use dotenv

--- src/com/library/db/DBConnection.java
+++ src/com/library/db/DBConnection.java
@@ -1,24 +1,21 @@
 package com.library.db;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-//import io.github.cdimascio.dotenv.Dotenv; // Import the library
 
 public class DBConnection {
     // Load the .env file
-//    private static final Dotenv dotenv = Dotenv.load();
+    private static final Dotenv dotenv = Dotenv.load();
 
     // Fetch the values using the keys you defined in the .env file
-//    private static final String URL = dotenv.get("DB_URL");
-//    private static final String USER = dotenv.get("DB_USER");
-//    private static final String PASS = dotenv.get("DB_PASS");
-
-    private static final String DB_URL = "jdbc:postgresql://localhost:5432/library";
-    private static final String DB_USER = "postgres";
-    private static final String DB_PASS = "Sayan123";
+    private static final String URL = dotenv.get("DB_URL");
+    private static final String USER = dotenv.get("USER");
+    private static final String PASS = dotenv.get("DB_PASS");
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+        return DriverManager.getConnection(URL, USER, PASS);
     }
 }
