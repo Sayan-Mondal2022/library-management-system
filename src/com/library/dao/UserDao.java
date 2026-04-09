99183b05c51e3253786c70a8e6e986b5aa0fc6db
Sayan-Mondal2022 <sayanmondal5678@gmail.com>
2026-04-09 12:22:02 +0530

    Implemented the function to add the user data into my User Table in DB

--- src/com/library/dao/UserDao.java
+++ src/com/library/dao/UserDao.java
@@ -5,25 +5,37 @@ import com.library.models.User;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 
 public class UserDao {
-    public int save(User user) {
+    // This function is used to save the user data into my DB, Completing the user registration process
+    public void save(User user) {
         Connection con = null;
-        int user_id = -1;
+        int user_id;
 
         try {
             // Have the DB connection established.
             con = DBConnection.getConnection();
 
-            String fetchEmailQuery = "SELECT email FROM Users WHERE email = ? LIMIT = 1";
-            String fetchPhoneNoQuery = "SELECT email FROM Users WHERE phone_no = ? LIMIT = 1";
+            // Once I get the User_id I will insert the data.
+            String sql = "INSERT INTO Users (user_name, address, phone_no, email, password_hash, user_type) VALUES (?, ?, ?, ?, ?, ?)";
 
+            try (PreparedStatement pstmt = con.prepareStatement(sql)) {
 
+                pstmt.setString(1, user.getUserName());
+                pstmt.setString(2, user.getAddress());
+                pstmt.setString(3, user.getPhoneNo());
+                pstmt.setString(4, user.getEmail());
+                pstmt.setString(5, user.getPasswordHash());
+                pstmt.setString(6, user.getUserType());
 
+                pstmt.executeUpdate();
 
-
-
+                System.out.println("User Data has been added to the Database!");
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         } catch (Exception e) {
             System.err.println("ERROR: Failed to build the DB connection" + e.getMessage());
         } finally {
@@ -35,7 +47,5 @@ public class UserDao {
                 }
             }
         }
-
-        return user_id;
     }
 }
